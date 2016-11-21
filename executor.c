c562471f8d69fbd05f1f740e78a50883c8a7107b
arvind <amahankali@stuy.edu>
2016-11-20 19:49:38 -0500

    took care of newline issue. it works now

--- executor.c
+++ executor.c
@@ -8,10 +8,21 @@ int main() {
 	char* command = (char *) calloc(1, 30);
 	printf("What do you want to do? ");
 	fgets(command, 25, stdin);
+
+	int i = 0;
+	for(; i < strlen(command); i++)
+	{
+		if(command[i] == '\n')
+		{
+			command[i] = 0;
+			break;
+		}
+	}
+
 	printf("\n");
 
 	char* args[10];
-	int i = 0;
+	i = 0;
 	char* originalCommand = command;
 	int len = strlen(command);
 	while(i < 10 && command - originalCommand <= strlen)
